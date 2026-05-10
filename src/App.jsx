dfa3fb356709ee804cad6b9d6a539ab01a439030
Anil Panda <anil.learn.programming@gmail.com>
2026-05-10 18:19:51 +0530

    implement welcome screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from "./components/Navbar";
-
+import Welcome from "./components/welcome";
 const App = () => {
   return( 
   <main>
     <Navbar/>
+    <Welcome/>
   </main>
   )
 }
 
-export default App;
+export default App
